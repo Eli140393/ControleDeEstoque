493f66357dd56bccad8a063eb6acd8606658cf3f
Eliezer <eli.defilicibus@gmail.com>
2021-09-26 08:55:39 -0300

    Login utilizando ADO-NET

--- ControleDeEstoque/ControleDeEstoque.Web/Controllers/ContaController.cs
+++ ControleDeEstoque/ControleDeEstoque.Web/Controllers/ContaController.cs
@@ -26,7 +26,7 @@ namespace ControleDeEstoque.Web.Controllers
                 return View(login);
             }
 
-            var achou = (login.Usuario == "eliezerdefilicibus" && login.Senha == "123");
+            var achou = UsuarioModel.ValidarUsuario(login.Usuario, login.Senha);
 
             if(achou)
             {
---
+++ ControleDeEstoque/ControleDeEstoque.Web/Models/UsuarioModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ControleDeEstoque.Web.Models
+{
+    public class UsuarioModel
+    {
+
+        public static bool ValidarUsuario(string login, string senha)
+        {
+            var ret = false;
+            using(var conexao = new SqlConnection())
+            {
+                conexao.ConnectionString = "Data Source=DESKTOP-KI43LUD\\SQLEXPRESS;Initial Catalog=DB_ControleEstoque;User Id=sa;Password=eliezer140393;";
+                conexao.Open();
+                using (var comando = new SqlCommand())
+                {
+                    comando.Connection = conexao;
+                    comando.CommandText = string.Format(
+                        "select count(*) from TB_Usuario where DS_Usuario = '{0}' and DS_Senha = '{1}' ", login,senha);
+                   ret=((int)comando.ExecuteScalar() > 0);
+                }
+            }
+
+            return ret;
+        }
+    }
+}
